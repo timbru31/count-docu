cec88866b989fc94c767b12440946e8df63735b5
stefan meyer <stefan.meyer@sinnerschrader.com>
2017-02-22 13:09:33 +0100

    fixed label

--- deploy.sh
+++ deploy.sh
@@ -56,6 +56,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $encrypted_c40f8a167d3c_key -iv $encrypted_c40f8a167d3c_iv -in count-docu-rssa.enc -out count-docu-rssa -d
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./count-docu-rssa.enc -out ./travis_rsa -d
 chmod 600 travis_rsa
 eval `ssh-agent -s`
